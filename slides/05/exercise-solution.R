891d8c4d5e70b24c417d8a7c3c2a2f2022fc953c
Amanda Luby <aluby@carleton.edu>
2025-04-14 12:03:16 -0500

    mon material

--- slides/05/exercise-solution.R
+++ slides/05/exercise-solution.R
@@ -1,6 +1,25 @@
 set.seed(0408)
 x <- rbeta(5, shape1 = 3, shape2 = 1)
 
+
+
+
+
+
+
+
+
+
+
+
+
+x <- c(.83, .49, .72, .57, .66)
+
+theta_hat <- (5 + sum(log(x)))/-sum(log(x))
+
+lik_func <- function(theta){(theta+1)^5 *prod(x^theta)}
+optimize(lik_func, interval = c(-20,20), maximum = TRUE)
+
 p3 <- tibble(
   theta = seq(0, 10, by = .01),
   log_lik = 5*log(theta+1) + theta*sum(log(x))
@@ -9,3 +28,4 @@ p3 <- tibble(
   geom_line() 
 
 ggplotly(p3)
+
